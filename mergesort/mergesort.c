dfd66cbd8b11bae4b06c0ce45e8323a5e8db521c
xion1547 <xion1547@morris.umn.edu>
2019-09-26 11:33:19 -0500

    Writes starter code for merge sort

--- mergesort/mergesort.c
+++ mergesort/mergesort.c
@@ -1,7 +1,24 @@
 #include "mergesort.h"
 
-void mergesort(int, int*) {
+void mergesort(int arrSize, int* array) {
   // This obviously doesn't actually do any *sorting*, so there's
   // certainly work still to be done.
-  return;
+  mergesortRange(array, 0, arrSize);
+  }
+
+void mergesortRange(int* array, int startIndex, int endIndex){
+  
+  int rangeSize = endIndex - startIndex;
+  if (rangeSize > 1) {
+  	int midPoint = (startIndex + endIndex) / 2;
+  	mergesortRange(array, startIndex, midPoint);
+	mergesortRange(array, midpoint + 1, endIndex);
+	mergeRanges(array, startIndex, midPoint, endIndex);
+  }
+}
+
+void mergeRanges(int* array, int startIndex, int midPoint, int endPoint){
+
+
+}
 }
