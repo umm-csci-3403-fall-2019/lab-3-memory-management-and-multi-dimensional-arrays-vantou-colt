e0a8135297b0e1d90b359a4f570eb94935e74586
ColtDahl <dahlcolt@gmail.com>
2019-10-01 11:03:13 -0500

    Finish mergesort implementation, begin work on memory deallocation

--- mergesort/mergesort.c
+++ mergesort/mergesort.c
@@ -1,28 +1,52 @@
 #include "mergesort.h"
 #include "stdlib.h"
 
+bool needsSorting(int rangeSize){
+       return rangeSize >= 2;
+       }
+
+
 void mergeRanges(int* array, int startIndex, int midPoint, int endPoint){
-	int rangeSize = endPoint = startIndex;
-	int* destination =  (int*)malloc(sizeof(int)*sizeof(rangeSize));
+	int rangeSize = endPoint - startIndex;
+	int* destination =  (int*)malloc(sizeof(int)*rangeSize);
 	int firstIndex = startIndex;
 	int secondIndex = midPoint;
 	int copyIndex = 0;
-	while(firstIndex < midPoint && secondIndex < endIndex){
-	if(values[firstIndex] < values[secondIndex]){
-		destination[copyIndex];
+	while(firstIndex < midPoint && secondIndex < endPoint){
+	if(array[firstIndex] < array[secondIndex]){
+		destination[copyIndex]=array[firstIndex];
 		++firstIndex;
 	}
-}
+	else{
+		destination[copyIndex] = array[secondIndex];
+		++secondIndex;
+	}
+	++copyIndex;
+	}
+	while(firstIndex < midPoint){
+	destination[copyIndex] = array[firstIndex];
+	++copyIndex;
+	++firstIndex;
+	}
+	while(secondIndex < endPoint){
+	destination[copyIndex] = array[secondIndex];
+	++copyIndex;
+	++secondIndex;
+	}
+	for(int i=0;i<rangeSize;++i){
+	array[i+startIndex] = destination[i];
+	}
 
+}
 
 
 void mergesortRange(int* array, int startIndex, int endIndex){
   
   int rangeSize = endIndex - startIndex;
-  if (rangeSize > 1) {
+  if (needsSorting(rangeSize)) {
   	int midPoint = (startIndex + endIndex) / 2;
   	mergesortRange(array, startIndex, midPoint);
-	mergesortRange(array, midPoint + 1, endIndex);
+	mergesortRange(array, midPoint, endIndex);
 	mergeRanges(array, startIndex, midPoint, endIndex);
   }
 }
